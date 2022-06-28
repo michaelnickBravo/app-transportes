8db1880116196e9a330ab6a09c7eea837f81b062
Roberto <231099montero@gmail.com>
2022-06-27 21:42:29 -0500

    Prueba

--- app/src/main/java/pe/edu/usat/laboratorio/appcomercial/adaptador/AdaptadorPrueba.java
+++ app/src/main/java/pe/edu/usat/laboratorio/appcomercial/adaptador/AdaptadorPrueba.java
@@ -1,4 +1,13 @@
 package pe.edu.usat.laboratorio.appcomercial.adaptador;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
 public class AdaptadorPrueba {
+
+    public AdaptadorPrueba(Context context) {
+        Toast.makeText(context, "PRUEBA", Toast.LENGTH_SHORT).show();
+    }
 }
